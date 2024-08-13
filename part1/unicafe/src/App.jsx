244e608adf599cfcb23db27fed4fee74e3229ac3
ruby waplington <r.m.waplington@gmail.com>
2024-08-13 21:50:19 +0100

    Unicafe 1.6 started

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,40 @@
 import { useState } from 'react'
 
+const Title = (props) => {
+  console.log('Title props: ', props)
+  const {text} = props
+
+  return (
+    <h2>{text}</h2>
+  )
+}
+
+const Button = (props) => { 
+  console.log('Button props: ', props)
+  const { handleClick, text } = props
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    
+  }
+
   return (
     <div>
-      code here
+      <h1>UniCafe Feedback :)</h1>
+      <Title text="Give Feedback"/>
+      <Button handleClick={handleGoodClick} text="good"/>
+      <Title text="Statistics"/>
     </div>
   )
 }
