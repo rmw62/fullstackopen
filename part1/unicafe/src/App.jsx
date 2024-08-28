aa1bdf75731cf184017fa3e82ec8a3a602512c32
ruby waplington <r.m.waplington@gmail.com>
2024-08-28 20:11:53 +0100

    Part 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -19,22 +19,60 @@ const Button = (props) => {
   )
 }
 
+const Statistics = (props) => {
+  console.log('Statistics props: ', props)
+  const {rating, ratingCount} = props
+  return (
+    <div>
+      {rating} {ratingCount}
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handleGoodClick = () => {
-    
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    console.log('good ', updatedGood)
+    const updatedAll = all + 1
+    setAll(updatedAll)
+    console.log('all ', updatedAll)
+  }
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    console.log('neutral ', updatedNeutral)
+    const updatedAll = all + 1
+    setAll(updatedAll)
+    console.log('all ', updatedAll)
+  }
+  const handleBadClick = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    console.log('bad ', updatedBad)
+    const updatedAll = all + 1
+    setAll(updatedAll)
+    console.log('all ', updatedAll)
   }
 
   return (
     <div>
       <h1>UniCafe Feedback :)</h1>
-      <Title text="Give Feedback"/>
-      <Button handleClick={handleGoodClick} text="good"/>
+      <Title text="Give Feedback" />
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
       <Title text="Statistics"/>
+      <Statistics rating="good" ratingCount={good} />
+      <Statistics rating="neutral" ratingCount={neutral} />
+      <Statistics rating="bad" ratingCount={bad} />
+      <Statistics rating="all" ratingCount={all} />
     </div>
   )
 }
